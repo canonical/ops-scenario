c53e475325a694eac6fb43a94e24e8f62b5da3ef
Pietro Pasotti <starfire.daemon@gmail.com>
2023-10-31 10:17:51 +0100

    lint

--- tests/test_snapshot/test_state_apply.py
+++ tests/test_snapshot/test_state_apply.py
@@ -2,9 +2,9 @@ import pytest
 
 from scenario import State
 from scenario.scripts.state_apply import (
+    JujuUnitName,
     _gather_juju_exec_cmds,
     _gather_raw_calls,
-    JujuUnitName,
 )
 
 
