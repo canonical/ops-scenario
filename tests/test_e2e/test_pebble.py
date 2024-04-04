5c8618acd08b5f6cf0af4494515bc25561a91e9b
Tony Meyer <tony.meyer@canonical.com>
2024-04-04 21:44:13 +1300

    Remove jsonpatch_delta from the public API.

--- tests/test_e2e/test_pebble.py
+++ tests/test_e2e/test_pebble.py
@@ -9,7 +9,7 @@ from ops.pebble import ExecError, ServiceStartup, ServiceStatus
 
 from scenario import Context
 from scenario.state import Container, ExecOutput, Mount, Port, State
-from tests.helpers import trigger
+from tests.helpers import jsonpatch_delta, trigger
 
 
 @pytest.fixture(scope="function")
@@ -158,7 +158,7 @@ def test_fs_pull(charm_cls, make_dirs):
     else:
         # nothing has changed
         out_purged = out.replace(stored_state=state.stored_state)
-        assert not out_purged.jsonpatch_delta(state)
+        assert not jsonpatch_delta(out_purged, state)
 
 
 LS = """
