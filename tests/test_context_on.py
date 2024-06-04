885c34af5f70624558ca425cc49765aac68d0c3e
Tony Meyer <tony.meyer@canonical.com>
2024-06-04 16:44:06 +1200

    Update tests and docs to match final (hopefully\!) API decision.

--- tests/test_context_on.py
+++ tests/test_context_on.py
@@ -153,10 +153,10 @@ def test_revision_secret_events_as_positional_arg(event_name):
         ("storage_detaching", ops.StorageDetachingEvent),
     ],
 )
-def test_storage_events(event_name, event_kind):
+def test_storage_events(as_kwarg, storage_index, event_name, event_kind):
     ctx = scenario.Context(ContextCharm, meta=META, actions=ACTIONS)
-    storage = scenario.Storage("foo")
-    state_in = scenario.State(storage=[storage])
+    storages = [scenario.Storage("foo", index) for index in range((storage_index or 0) + 1)]
+    state_in = scenario.State(storage=storages)
     # These look like:
     #   ctx.run(ctx.on.storage_attached(storage), state)
     with ctx.manager(getattr(ctx.on, event_name)(storage), state_in) as mgr:
@@ -165,8 +165,17 @@ def test_storage_events(event_name, event_kind):
         assert isinstance(mgr.charm.observed[1], ops.CollectStatusEvent)
         event = mgr.charm.observed[0]
         assert isinstance(event, event_kind)
-        assert event.storage.name == storage.name
-        assert event.storage.index == storage.index
+        assert event.storage.name == storages[-1].name
+        assert event.storage.index == storages[-1].index
+
+
+@pytest.mark.parametrize("event_name", ["storage_attached", "storage_detaching"])
+def test_storage_events_as_positional_arg(event_name):
+    ctx = scenario.Context(ContextCharm, meta=META, actions=ACTIONS)
+    storage = scenario.Storage("foo")
+    state_in = scenario.State(storage=[storage])
+    with pytest.assertRaises(ValueError):
+        ctx.run(getattr(ctx.on, event_name)(storage, 0), state_in)
 
 
 def test_action_event_no_params():
@@ -309,9 +318,7 @@ def test_relation_unit_events(event_name, event_kind):
     state_in = scenario.State(relations=[relation])
     # These look like:
     #   ctx.run(ctx.on.baz_relation_changed(unit=unit_ordinal), state)
-    with ctx.manager(
-        getattr(ctx.on, event_name)(relation, remote_unit=2), state_in
-    ) as mgr:
+    with ctx.manager(getattr(ctx.on, event_name)(relation, unit=2), state_in) as mgr:
         mgr.run()
         assert len(mgr.charm.observed) == 2
         assert isinstance(mgr.charm.observed[1], ops.CollectStatusEvent)
